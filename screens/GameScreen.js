3c6a2fa7035cc55ae13791e2725371f93d6ce6b1
Daanish Millwalla <dmillwalla@gmail.com>
2021-10-22 16:36:30 +0100

    [fix] interval looping

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,48 +1,50 @@
-import React, {useEffect, useState} from 'react'
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-const dirs = ['L', 'T']
-let left; let right; let top; let bottom;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const dirs = ["L", "T"];
+let left;
+let right;
+let top;
+let bottom;
 
 const GameScreen = () => {
-    //console.log(windowHeight, windowWidth);
-    const [randDir, setRandDir] = useState(0)
-    const [count, setCount] = useState(0)
-    const [left, setLeft] = useState();
-    const [top, setTop] = useState();
-    useEffect(() => {
-        setRandDir(dirs[Math.floor(Math.random() * dirs.length)])
-        console.log(randDir)
-        setCount(count + 1);
-        
-      }, [count]);
-    
-    console.log('SEDAT');
-    console.log(count)
-    const boundaryCheck = () => 
-    {
-        
-    }
+  //console.log(windowHeight, windowWidth);
+  const [randDir, setRandDir] = useState(0);
+  const [count, setCount] = useState(0);
+  const [left, setLeft] = useState();
+  const [top, setTop] = useState();
+  const [time, setTime] = useState(Date.now());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("random direction", randDir);
+      console.log("random index", Math.floor(Math.random() * dirs.length));
+      setRandDir(dirs[Math.floor(Math.random() * dirs.length)]);
+      setTime(Date.now());
+    }, 1);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-    return (
-        <View>
-            <View removeClippedSubviews={false} style={styles.gameBoard}></View>
-        </View>
-    )
-}
+  const boundaryCheck = () => {};
 
-export default GameScreen
+  return (
+    <View>
+      <View removeClippedSubviews={false} style={styles.gameBoard}></View>
+    </View>
+  );
+};
 
-
+export default GameScreen;
 
 const styles = StyleSheet.create({
-    gameBoard:{
-        height: 200,
-        width: 200,
-        borderWidth: 2,
-        top:50,
-        left: 100
-    }
-})
+  gameBoard: {
+    height: 200,
+    width: 200,
+    borderWidth: 2,
+    top: 50,
+    left: 100,
+  },
+});
